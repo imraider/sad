59d69d46eb66454a59f1216ebe2d983620f6ec01
imraider <45240051+imraider@users.noreply.github.com>
2019-01-03 15:28:55 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,336 @@
+const moment = require('moment')
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");//youtube api key
+const queue = new Map();//the queue map
+const token = process.env.BOT_TOKEN//your bot token
+const prefix = '+'//your prefix
+
+
+//All copytights for Eyad & (old) Codes
+
+/*
+Packages
+npm install discord.js
+npm install ytdl-core
+npm install get-youtube-id
+npm install youtube-info
+npm install simple-youtube-api
+npm install queue
+*/
+
+client.on('ready', () => {//the ready msgs
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`in ${client.guilds.size} servers `)
+    console.log(`[ ] ${client.users.size}`)
+});
+
+client.on('message', async msg => {
+	if (msg.author.bot) return undefined;
+	if (!msg.content.startsWith(prefix)) return undefined;
+	const args = msg.content.split(' ');
+	const searchString = args.slice(1).join(' ');
+	const url = args[1] ? args[1] .replace(/<(.+)>/g, '$1') : '';
+	const serverQueue = queue.get(msg.guild.id);
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+	if (command === `play`) {
+		const voiceChannel = msg.member.voiceChannel;
+		if (!voiceChannel) return msg.channel.send('??? ????? ????? ???? ???? .');
+		const permissions = voiceChannel.permissionsFor(msg.client.user);
+		if (!permissions.has('CONNECT')) {
+			return msg.channel.send('?? ?????? ??? ?????? ?????? ???? ?????');
+		}
+		if (!permissions.has('SPEAK')) {
+			return msg.channel.send('?? ?????? ??? ?????? ?????? ???? ?????');
+		}
+
+		if (!permissions.has('EMBED_LINKS')) {
+			return msg.channel.sendMessage("**??? ????? ????? `EMBED LINKS`??? **rl")
+			}
+
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+			const playlist = await youtube.getPlaylist(url);
+			const videos = await playlist.getVideos();
+			for (const video of Object.values(videos)) {
+				const video2 = await youtube.getVideoByID(video.id);
+				await handleVideo(video2, msg, voiceChannel, true);
+			}
+			return msg.channel.send(` **${playlist.title}** ?? ??????? ??? ????? ???????`);
+		} else {
+			try {
+
+				var video = await youtube.getVideo(url);
+
+			} catch (error) {
+				try {
+				    					    var fast = {};
+					var videos = await youtube.searchVideos(searchString, 10);
+					let index = 0;
+					const embed1 = new Discord.RichEmbed()
+			        .setDescription(`**?????? ?? ????? ?????? ??? ??????** :
+${videos.map(video2 => `[**${++index}**] **${video2.title}**`).join('\n')}`)
+					.setFooter(`${msg.guild.name}`)
+					msg.channel.sendEmbed(embed1).then(message =>{
+
+						message.delete(15000)
+
+					});
+					try {
+						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+							maxMatches: 1,
+							time: 20000,
+							errors: ['time']
+						})
+
+						}catch(err) {
+						console.error(err);
+						return msg.channel.send('?? ??? ?????? ???? ????');
+						}
+					const videoIndex = parseInt(response.first().content);
+					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+				} catch (err) {
+					console.error(err);
+					return msg.channel.send(':x: ?? ????? ????? ??? ');
+				}
+		}
+
+			return handleVideo(video, msg, voiceChannel);
+		}
+	} else if (command === `skip`) {
+		if (!msg.member.voiceChannel) return msg.channel.send('??? ??? ???? ???? .');
+		if (!serverQueue) return msg.channel.send('?? ????? ???? ???????');
+		serverQueue.connection.dispatcher.end('?? ????? ??? ??????');
+		return undefined;
+	} else if (command === `stop`) {
+		if (!msg.member.voiceChannel) return msg.channel.send('??? ??? ???? ???? .');
+		if (!serverQueue) return msg.channel.send('?? ????? ???? ???????');
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end('?? ????? ??? ??????');
+		return undefined;
+	} else if (command === `vol`) {
+		if (!msg.member.voiceChannel) return msg.channel.send('??? ??? ???? ???? .');
+		if (!serverQueue) return msg.channel.send('?? ???? ??? ????.');
+		if (!args[1]) return msg.channel.send(`:loud_sound: ????? ????? **${serverQueue.volume}**`);
+		serverQueue.volume = args[1];
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+		return msg.channel.send(`:speaker: ?? ???? ????? ??? **${args[1]}**`);
+	} else if (command === `np`) {
+		if (!serverQueue) return msg.channel.send('?? ???? ??? ???? ? ?????.');
+		const embedNP = new Discord.RichEmbed()
+	.setDescription(`:notes: ???? ??? ????? : **${serverQueue.songs[0].title}**`)
+		return msg.channel.sendEmbed(embedNP);
+	} else if (command === `replay`) {
+		if (!serverQueue) return msg.channel.send('?? ???? ??? ???? ? ?????.');
+		const embedNP = new Discord.RichEmbed()
+	.setDescription(`???? ????? ????? ?????? :**${serverQueue.songs[0].title}**`)
+	msg.channel.send({embed: embedNP})
+  	 return handleVideo(video, msg, msg.member.voiceChannel);
+
+	} else if (command === `queue`) {
+		if (!serverQueue) return msg.channel.send('?? ???? ??? ???? ? ?????.');
+		let index = 0;
+		const embedqu = new Discord.RichEmbed()
+.setDescription(`**Songs Queue**
+${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
+**???? ??? ?????** ${serverQueue.songs[0].title}`)
+		return msg.channel.sendEmbed(embedqu);
+	} else if (command === `pause`) {
+		if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			return msg.channel.send('?? ????? ???????? ?????!');
+		}
+		return msg.channel.send('?? ???? ??? ???? ? ?????.');
+	} else if (command === "resume") {
+		if (serverQueue && !serverQueue.playing) {
+			serverQueue.playing = true;
+			serverQueue.connection.dispatcher.resume();
+			return msg.channel.send('??????? ???????? ??????? ?? !');
+		}
+		return msg.channel.send('?? ???? ??? ???? ?? ?????.');
+	}
+
+	return undefined;
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+	const serverQueue = queue.get(msg.guild.id);
+	const song = {
+		id: video.id,
+		title: Util.escapeMarkdown(video.title),
+		url: `https://www.youtube.com/watch?v=${video.id}`,
+		time:`${video.duration.hours}:${video.duration.minutes}:${video.duration.seconds}`,
+		eyad:`${video.thumbnails.high.url}`,
+		best:`${video.channel.title}`,
+		bees:`${video.raw.snippet.publishedAt}`,
+		shahd:`${video.raw.kind}`,
+		zg:`${video.raw.snippet.channelId}`,
+        views:`${video.raw.views}`,
+        like:`${video.raw.likeCount}`,
+        dislike:`${video.raw.dislikeCount}`,
+        hi:`${video.raw.id}`
+	};
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: msg.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+		queue.set(msg.guild.id, queueConstruct);
+		queueConstruct.songs.push(song);
+		try {
+			var connection = await voiceChannel.join();
+			queueConstruct.connection = connection;
+			play(msg.guild, queueConstruct.songs[0]);
+		} catch (error) {
+			console.error(`I could not join the voice channel: ${error}`);
+			queue.delete(msg.guild.id);
+			return msg.channel.send(`?? ?????? ???? ??? ????? ${error}`);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		if (playlist) return undefined;
+		else return msg.channel.send(` **${song.title}** ?? ????? ??????? ??? ???????!`);
+	}
+	return undefined;
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	console.log(serverQueue.songs);
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', reason => {
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+			else console.log(reason);
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => console.error(error));
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+		fetchVideoInfo(`${song.hi}`, function (err,  idk) {
+  if (err) throw new Error(err);
+  console.log( idk);
+  	  const yyyy = {}
+  if(!yyyy[msg.guild.id]) yyyy[msg.guild.id] = {
+    like: `${ idk.likeCount}`,
+    dislike: `${ idk.dislikeCount}`
+  }
+	serverQueue.textChannel.send({embed : new Discord.RichEmbed()
+  .setTitle(`**${ idk.title}**`)
+  .setURL( idk.url)
+  .addField('Time The Video :' , `${song.time}`, true)
+  .addField('Channel Name :' , `${song.best}`, true)
+  .addField('Channel ID :' , `${song.zg}`, true)
+  .addField('Video Created at :' , `${ idk.datePublished}`, true)
+  .addField('Views :' , `${ idk.views}`, true)
+  .addField('Like?? :' , `${ idk.likeCount}`, true)
+  .addField('dislike?? :' , `${ idk.dislikeCount}`, true)
+  .addField('comments :' , `${ idk.commentCount}`, true)
+	.setImage(`${song.eyad}`)
+	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
+	.setColor('#ff0000')
+	.setTimestamp()
+	}).then(love => {
+		love.react('??').then(r=>{
+		love.react('??').then(r =>{
+		love.react('??').then(r=> {
+    let likee = (reaction, user) => reaction.emoji.name === '??' && user.id === msg.author.id;
+    let dislikee = (reaction, user) => reaction.emoji.name === '??' && user.id === msg.author.id;
+    let cnn = (reaction, user) => reaction.emoji.name === '??' && user.id === msg.author.id;
+
+    let ll = love.createReactionCollector(likee , {max:5});
+    let dd = love.createReactionCollector(dislikee , {max:5});
+    let cn = love.createReactionCollector(cnn , {max:5});
+
+		    ll.on("collect", r => {
+		      yyyy[msg.guild.id].like++;
+	love.edit({embed : new Discord.RichEmbed()
+  .setTitle(`**${ idk.title}**`)
+  .setURL( idk.url)
+  .addField('Time The Video :' , `${song.time}`, true)
+  .addField('Channel Name :' , `${song.best}`, true)
+  .addField('Channel ID :' , `${song.zg}`, true)
+  .addField('Video Created at :' , `${ idk.datePublished}`, true)
+  .addField('Views :' , `${ idk.views}`, true)
+  .addField('Like?? :' , `${yyyy[msg.guild.id].like}`, true)
+  .addField('dislike?? :' , `${ idk.dislikeCount}`, true)
+  .addField('comments :' , `${ idk.commentCount}`, true)
+	.setImage(`${song.eyad}`)
+	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
+	.setColor('#ff0000')
+	.setTimestamp()
+});
+    })
+
+    dd.on("collect", r => {
+      yyyy[msg.guild.id].dislike++;
+	love.edit({embed : new Discord.RichEmbed()
+  .setTitle(`**${ idk.title}**`)
+  .setURL( idk.url)
+  .addField('Time The Video :' , `${song.time}`, true)
+  .addField('Channel Name :' , `${song.best}`, true)
+  .addField('Channel ID :' , `${song.zg}`, true)
+  .addField('Video Created at :' , `${ idk.datePublished}`, true)
+  .addField('Views :' , `${ idk.views}`, true)
+  .addField('Like?? :' , `${ idk.likeCount}`, true)
+  .addField('dislike?? :' , `${yyyy[msg.guild.id].dislike}`, true)
+  .addField('comments :' , `${ idk.commentCount}`, true)
+	.setImage(`${song.eyad}`)
+	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
+	.setColor('#ff0000')
+	.setTimestamp()
+});
+})
+    cn.on("collect", r => {
+	love.edit({embed : new Discord.RichEmbed()
+  .setTitle(`**${ idk.title}**`)
+  .setURL( idk.url)
+  .addField('Time The Video :' , `${song.time}`, true)
+  .addField('Channel Name :' , `${song.best}`, true)
+  .addField('Channel ID :' , `${song.zg}`, true)
+  .addField('Video Created at :' , `${ idk.datePublished}`, true)
+  .addField('Views :' , `${ idk.views}`, true)
+  .addField('Like?? :' , `${ idk.likeCount}`, true)
+  .addField('dislike?? :' , `${ idk.dislikeCount}`, true)
+  .addField('comments :' , `${ idk.commentCount}`, true)
+	.setImage(`${song.eyad}`)
+	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
+	.setColor('#ff0000')
+	.setTimestamp()
+});
+})
+})
+})
+})
+})
+})
+}
+});
+
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`1play | Legend Raider`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
